c1aedc2bb90875beeb8d7d5e159552665f63b54c
MattheKnorr <matt@knorrs.net>
2026-09-19 00:06:36 -0600

    Finalized Week 1

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,24 +1,21 @@
-const routeOne = (req, res) => {
+const home = (req, res) => {
   res.send(`<div style="font-family:Arial;text-align:center;margin-top:20vh;">
     <h1 style="color:royalblue;font-size:64px;margin:0;">Matthew Knorr!</h1>
     <h2 style="color:#555;font-weight:normal;">Software Development</h2>
+    <h3><a href="./week01" style="color:#555;text-decoration:none;font-weight:500;border-bottom:2px solid royalblue;padding-bottom:3px;">Week 01</a></h3>
   </div>`);
 };
 
-const routeTwo = (req, res) => {
+const week01 = (req, res) => {
   res.send(`<div style="font-family:Arial;text-align:center;margin-top:20vh;">
-    <h1 style="color:royalblue;font-size:64px;margin:0;">Route Two!</h1>
-  </div>`);
-};
-
-const routeThree = (req, res) => {
-  res.send(`<div style="font-family:Arial;text-align:center;margin-top:20vh;">
-    <h1 style="color:royalblue;font-size:64px;margin:0;">Route Three!</h1>
+    <h1 style="color:royalblue;font-size:64px;margin:0;">Week 01!</h1>
+    <h2> <a href="https://youtu.be/pUc5d4er6Fo" 
+    style="color:#555;text-decoration:none;font-weight:500;border-bottom:2px solid royalblue;padding-bottom:3px;"> Video Submission </a></h2>
   </div>`);
 }
 
+
 module.exports = {
-  routeOne,
-  routeTwo,
-  routeThree
+  home,
+  week01
 };  
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
 
-routes.get('/', lesson1Controller.routeOne);
-routes.get('/routeTwo', lesson1Controller.routeTwo);
-routes.get('/routeThree', lesson1Controller.routeThree);
+routes.get('/', lesson1Controller.home);
+routes.get('/week01', lesson1Controller.week01);
 
 module.exports = routes;
